0584abaec994328e8b3ef0bdb97612abcc368797
Mukhammet Rakhymov <49809953+DLWHI@users.noreply.github.com>
2020-01-04 20:05:52 +0300

    Added configuration
    Added saving last requested schedule to configuration and uploading it at start

---
+++ SceduleMephi/Configurator.cs
@@ -0,0 +1,33 @@
+﻿using SceduleLoader.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace SceduleLoader.SceduleMephi
+{
+    class Configurator : IConf
+    {
+        Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+        public List<string> GetSchedule(string dayOfWeek)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Read(string data)
+        {
+            ConfigurationManager.RefreshSection("appSettings");
+            foreach (KeyValueConfigurationElement info in config.AppSettings.Settings)
+                if (info.Key == data)
+                    return info.Value;
+            return "Нет таких данных";
+        }
+
+        public void Save(string Key, string Value)
+        {
+            config.AppSettings.Settings[Key].Value = Value;
+            config.Save();
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
